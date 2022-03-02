1d205d5768499e42e33150ebcef8d2f13de80b57
Pavel Ermolin <pavel.ermolin@sap.com>
2022-03-02 14:47:11 +0100

    Apply some refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HomeComponent} from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    HomeComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        BrowserAnimationsModule
+    ],
+    declarations: [
+        AppComponent,
+        TopBarComponent,
+        HomeComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,11 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  host: {
-    class:'home-container'
-  },
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  @HostBinding('class') class = 'home-container-1';
-  
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onToProductsClick() {
-    
-  }
-
+export class HomeComponent {
+    constructor() {
+    }
 }
