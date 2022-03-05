cf94a1d6e349b73979f07e6f6c8c70204b439be1
Turek <bartosz.turek@sap.com>
2022-03-05 20:04:23 +0100

    project push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,25 @@ import {TopBarComponent} from './top-bar/top-bar.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HomeComponent} from './home/home.component';
 import {AppRoutingModule} from './app-routing.module';
+import { ProductListComponent } from './product-list/product-list.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
     imports: [
         BrowserModule,
         ReactiveFormsModule,
         AppRoutingModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        RouterModule.forRoot([
+            { path: 'products', component: ProductListComponent},
+            { path: 'home', component: HomeComponent}
+        ])
     ],
     declarations: [
         AppComponent,
         TopBarComponent,
-        HomeComponent
+        HomeComponent,
+        ProductListComponent
     ],
     bootstrap: [
         AppComponent
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {productService} from "../product.service";
+import {Product} from "../models/Product";
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  providers: [productService]
+})
+export class ProductListComponent implements OnInit{
+  pageTitle: string = 'Product List';
+  products: Product[] = [];
+  currency: boolean = false;
+
+  constructor(private productService: productService) {
+  }
+
+  ngOnInit() {
+    this.products = this.productService.getProducts();
+  }
+
+  currencySwitch(): void{
+    this.currency = !this.currency;
+  }
+
+}
